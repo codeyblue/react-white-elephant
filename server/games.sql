0666cfa1ccab389e8142b0b47ef987ad82b40104
Kyla Kolb <kkolb416@gmail.com>
2022-09-29 10:44:32 -0400

    add users table to database

--- server/games.sql
+++ server/games.sql
@@ -1,10 +1,11 @@
 CREATE TABLE `games` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int AUTO_INCREMENT,
   `administrator` int NOT NULL,
   `status` enum('setup','ready','inprogress','completed') DEFAULT 'setup',
-  `rule_maxstealsperpresent` int DEFAULT NULL,
-  `rule_maxstealsperround` int NOT NULL,
-  `rule_firstpersonsecondchance` tinyint(1) NOT NULL,
-  `rule_extraround` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
+  `rule_maxstealsperpresent` int DEFAULT 3,
+  `rule_maxstealsperround` int DEFAULT -1,
+  `rule_firstpersonsecondchance` bool DEFAULT true,
+  `rule_extraround` bool DEFAULT true,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`administrator`) REFERENCES users(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- server/presents.sql
+++ server/presents.sql
@@ -1,9 +1,11 @@
 CREATE TABLE `presents` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'primary key',
-  `gifter` varchar(255) NOT NULL COMMENT 'person who brought gift',
+  `id` int AUTO_INCREMENT COMMENT 'primary key',
+  `gifter` int NOT NULL COMMENT 'person who brought gift',
   `status` enum('wrapped','open','locked') DEFAULT 'wrapped' COMMENT 'status of the present either wrapped, open, or locked',
   `holder` int DEFAULT NULL COMMENT 'user id of person currently holding the present',
   `game_key` int NOT NULL COMMENT 'reference to game id the present belongs to',
   PRIMARY KEY (`id`),
+  FOREIGN KEY (`gifter`) REFERENCES users(`id`),
+  FOREIGN KEY (`holder`) REFERENCES users(`id`),
   FOREIGN KEY (`game_key`) REFERENCES games(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
