5e731f27e77feab22883e5cd614849fafeffa28c
Kyla Kolb <kkolb416@gmail.com>
2022-10-28 00:36:53 -0400

    ability to delete a present

--- client/src/components/Presents/ViewPresent.js
+++ client/src/components/Presents/ViewPresent.js
@@ -1,13 +1,9 @@
 import EditPresent from './EditPresent';
 
-// todo add ability to delete present
-
 const ViewPresent = ({ presentData, gameData, setModalState, user, games }) => {
   const items = presentData.items.map(item => {
     return <div key={`item-${item.id}`}>
-      {item.img && <img src={item.img} alt={`img-${item.id}`} />}
       <p>{item.description}</p>
-      {item.url && <a href={item.url} target='_blank'>View</a>}
     </div>;
   });
 
@@ -41,6 +37,7 @@ const ViewPresent = ({ presentData, gameData, setModalState, user, games }) => {
     <p>Game {`${gameData.id}`} ({`${gameData.status}`})</p>
     {items}
     <button onClick={(e) => handleEditPresent(e)}>Edit</button>
+    <button onClick={(e) => handleDeletePresent(e)}>Delete</button>
   </>;
 };
 
